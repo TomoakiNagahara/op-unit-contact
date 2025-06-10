ecb23a4eb5339552e754cc96180ce8e79706a66a
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-10 11:15:22 +0900

    New: Contact :: QQL() : OP\IF_QQL

--- Contact.class.php
+++ Contact.class.php
@@ -89,4 +89,26 @@ class Contact implements IF_UNIT
 		//	Return the IF_FORM.
 		return $_form;
 	}
+
+	/** QQL
+	 *
+	 * @created    2025-06-10
+	 * @return    \OP\IF_QQL
+	 */
+	static function QQL() : \OP\IF_QQL
+	{
+		/* @var $_qql \OP\IF_QQL */
+		static $_qql = null;
+
+		//	Instantiate the unit only once.
+		if( $_qql === null ){
+			$_qql = OP()->Unit('QQL');
+			$file = 'Contact.sqlite3';
+			$path = (OP()->Env()->isCI() ? 'ci/':'') . $file;
+			$_qql->Open($path);
+		}
+
+		//	Return the IF_QQL.
+		return $_qql;
+	}
 }
